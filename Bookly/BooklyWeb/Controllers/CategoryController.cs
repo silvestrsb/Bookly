836613265953dcdab0dd1111b2f0f1baf3a24f91
Silvestrs <53860029+silvestrsb@users.noreply.github.com>
2022-11-20 14:08:30 +0200

    Initial Bookly project files, dbContext, Category page

---
+++ Bookly/BooklyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BooklyWeb.Data;
+using BooklyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooklyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _dbContext.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
